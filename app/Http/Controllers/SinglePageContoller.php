f37788d10d21221be6f01ce0d5ad75d553e64d9a
douglasrono <douglasronno@gmail.com>
2023-02-21 19:41:42 +0300

    add single page controller

---
+++ app/Http/Controllers/SinglePageContoller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SinglePageContoller extends Controller
+{
+    public function index(){
+
+        dd('hello');
+
+    }
+}
