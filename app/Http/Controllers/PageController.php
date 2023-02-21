ee71beb92daf55d4e6846eb6fdff8a09f9dc13fd
douglasrono <douglasronno@gmail.com>
2023-02-21 19:37:07 +0300

    added portfolio details logic

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -41,4 +41,9 @@ class PageController extends Controller
     {
         return view('contact');
     }
+
+    public function portfolioDetail()
+    {
+        return view('portfolioDetail');
+    }
 }
